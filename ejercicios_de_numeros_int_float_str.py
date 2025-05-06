9c7a806ea03e130544b753363bacd619d64431df
Jairo-86 <jairomorasanchez@gmail.com>
2025-05-06 12:38:05 +0200

    Añadir ejercicios con números enteros, flotantes y cadenas

---
+++ ejercicios_de_numeros_int_float_str.py
@@ -0,0 +1,18 @@
+# Primer ejercicio
+print("¿Para que el resultado sea 50, cuál debe ser el número que falta?")
+print("25 se debe multiplicar por? ")
+multiplicar = float(input(""))  # Convertimos la entrada a un número decimal (float)
+resultado = 25 * multiplicar  # Operación de multiplicación
+
+# Mostrar el resultado
+print("Si multiplicas " + str(multiplicar) + " por 25 el resultado es " + str(resultado))
+
+# Segundo ejercicio
+print("¿Para que el resultado sea 7.5, cuál debe ser el número que falta?")
+print("3 se debe multiplicar por? ")
+multiplicar = float(input(""))  # Convertimos la entrada a un número decimal (float)
+numero_con_decimales = 3  # Este es el número que se va a multiplicar
+resultado = multiplicar * numero_con_decimales  # Operación de multiplicación
+
+# Mostrar el resultado
+print("Si multiplicas " + str(multiplicar) + " por " + str(numero_con_decimales) + " el resultado es " + str(resultado))
